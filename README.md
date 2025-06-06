8ddf9c05573dbb3cb76041a86cbaee328ef54211
TheKankan <quentin.lejuez@gmail.com>
2025-06-06 18:01:55 +0200

    textnodes finished

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # website_gen
+
+A program used to generate static site in **python**
+
+I built this program during a course on Boot.dev
--- src/htmlnode.py
+++ src/htmlnode.py
@@ -19,3 +19,36 @@ class HTMLNode():
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("Leaf Nodes must have value")
+        
+        if self.tag is None:
+            return self.value
+
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Parent Nodes must have a tag")
+        
+        if self.children is None or self.children == []:
+            raise ValueError("Parent Nodes must have children")
+
+        result = f"<{self.tag}{self.props_to_html()}>"
+        for node in self.children:
+            result = result + node.to_html()
+        result = result + f"</{self.tag}>"
+        return result
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
