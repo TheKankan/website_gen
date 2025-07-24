55f1f5de4d4ffee324211490f39213b84d50c29b
TheKankan <quentin.lejuez@gmail.com>
2025-07-24 17:19:53 +0200

    added css and images

--- src/main.py
+++ src/main.py
@@ -22,7 +22,7 @@ def main():
         shutil.rmtree(dir_path_public)
 
     print("Copying static files to public directory...")
-    copy_content(dir_path_public, dir_path_public)
+    copy_content(dir_path_static, dir_path_public)
 
     generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
